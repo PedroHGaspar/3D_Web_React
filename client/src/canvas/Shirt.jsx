a41c3401922bd9a73ec39d7ecc32d333be8903d2
Pedro Gaspar <ordep12@hotmail.com.br>
2023-04-20 10:13:59 -0300

    Now we have the shirt, the logo and the texture.

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio'
+import { useFrame } from '@react-three/fiber';
+import { useGLTF, useTexture, Decal } from '@react-three/drei';
+
+
+import state from '../store'
+
+const Shirt = () => {
+
+    const snap = useSnapshot(state)
+    const { nodes, materials } = useGLTF('/shirt_baked.glb')
+    const logoTexture = useTexture(snap.logoDecal)
+    const fullTexture = useTexture(snap.fullDecal)
+
+    return (
+        <group>
+            <mesh
+                castShadow
+                geometry={nodes.T_Shirt_male.geometry}
+                material={materials.lambert1}
+                material-roughness={1}
+                dispose={null}
+            >
+                {snap.isFullTexture && (
+                    <Decal
+                        position={[0, 0, 0]}
+                        rotation={[0, 0, 0]}
+                        scale={1}
+                        map={fullTexture}
+                    />
+                )}
+                {snap.isLogoTexture && (
+                    <Decal
+                        position={[0, 0.04, 0.15]}
+                        rotation={[0, 0, 0]}
+                        scale={0.15}
+                        map={logoTexture}
+                    />
+                )}
+            </mesh>
+        </group>
+    )
+}
+
+export default Shirt
